77182f810f44c14f45fbd2548082d599ba5d85b4
michael.bradshaw <michael.bradshaw@centre.edu>
2019-02-07 15:43:25 -0500

    cool adventure!

--- js/adventure.js
+++ js/adventure.js
@@ -1,3 +1,26 @@
 
-var t =document.getElementById("title");
-t.innerText = "I like you <3";
+
+var nicer = function()
+{
+  var t =document.getElementById("title");
+  t.innerText = "I like you <3";
+
+  var p = document.getElementById("par1");
+  p.setAttribute("onclick","step1()");
+}
+var step1 = function()
+{
+  var p = document.getElementById("par1");
+  p.innerText="Stay with me ;(";
+
+  var fireButton = document.getElementById("fireButton");
+  fireButton.removeAttribute("disabled");
+}
+var fire = function()
+{
+  var fireButton = document.getElementById("fireButton");
+  var p = document.createElement("p");
+  p.innerText="Please don't Fire";
+  fireButton.after(p);
+
+}
